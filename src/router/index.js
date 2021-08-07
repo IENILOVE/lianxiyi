66a36819fb3839f535ec7182fec8212af37cc74a
IENILOVE <2706925712@qq.com>
2021-08-07 18:43:32 +0800

    第二次提交

---
+++ src/router/index.js
@@ -0,0 +1,18 @@
+import VueRouter from 'vue-router'
+import Vue from 'vue'
+
+//1 通过Vue.use() 安装插件
+Vue.use(VueRouter)
+
+
+//2 创建VueRouter对象
+const routers = [
+
+]
+const router = new VueRouter({
+    routers
+})
+
+
+//3 将router对象传入到Vue实例
+export default router
